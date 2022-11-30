6f8e9985ba66962a2478d2a36c99b25b65a7ca90
YiJohnZhang <yijohnzhang@gmail.com>
2022-11-29 23:42:42 -0800

    fin `04_39.09.11`
    completed extra study except the "style your app so that it looks nice"

--- 04_39.09.11_react_effects_refs/src/Cards.js
+++ 04_39.09.11_react_effects_refs/src/Cards.js
@@ -6,7 +6,8 @@ function Cards(){
 	let deckIDRef = useRef(null);
 	let timerIntervalID = useRef();
 	const cardImageContainer = useRef();
-	const [getNextCard, setGetNextCard] = useState(0);
+	const [cardsRemaining, setCardsRemaining] = useState(52);
+	const [drawCardFlag, setDrawCardFlag] = useState(false);
 	const [autoDraw, setAutoDraw] = useState(false);
 	const [shuffleDeckFlag, setShuffleDeckFlag] = useState(false);
 
@@ -14,13 +15,12 @@ function Cards(){
 	useEffect(() => {
 
 		cardImageContainer.current.innerHTML = '';
-		setGetNextCard(1);
+		setCardsRemaining(52);
 
 		async function getAPIDeckID(){
 
 			const response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 			deckIDRef.current = response.data.deck_id;
-			// console.log(deckIDRef);
 
 		}
 
@@ -33,74 +33,61 @@ function Cards(){
 
 		async function drawDeck(){
 
-			// console.log(deckIDRef);
-			// console.log(getNextCard);
 			const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckIDRef.current}/draw/?count=1`);
-			// console.log(response.data);
 
-			// const newCardIndex = getNextCard-1;
 			const newCardImage = document.createElement('img');
 			newCardImage.src = response.data.cards[0].image;
 			newCardImage.alt = response.data.cards[0].code;
+			setCardsRemaining(response.data.remaining);
 
 			cardImageContainer.current.appendChild(newCardImage)
 			
 		}
 		
-		// console.log(deckIDRef)
 		if(deckIDRef.current)	// prevent race conditions with the `getAPIDeckID`
 			drawDeck();
 
-	}, [getNextCard]);
+	}, [drawCardFlag]);
 
 	// autodraw next card from API
 	useEffect(() => {
 
 		if(autoDraw){
+		
+			timerIntervalID.current = setInterval(drawNextCard, 1000);
+		
+		}else{
 
-
+			clearInterval(timerIntervalID.current);
 
 		}
 
-
-		return () => {
-
-
-
-		};
-
 	}, [autoDraw]);
 	
-	function drawNextCard(evt){
-		
-		evt.preventDefault();
-		setGetNextCard((count) => count + 1);
+	function drawNextCard(){
 
+		setDrawCardFlag((drawCardFlag) => !drawCardFlag);
 
 	}
 
-	function toggleAutoDraw(evt){
+	function toggleAutoDraw(){
 
-		evt.preventDefault();
 		setAutoDraw((currentValue) => !currentValue);
 
 	}
 
-	function shuffleDeck(evt){
-
-		evt.preventDefault();
+	function shuffleDeck(){
+		
 		setShuffleDeckFlag((currentFlag) => !currentFlag);
 
 	}
 
 	return(
 	<>
-		<form>
-			<button onClick={drawNextCard}>Draw a Card</button>
-			<button onClick={toggleAutoDraw}>Toggle Auto-Draw</button>
-			<button onClick={shuffleDeck}>Get a New Deck</button>
-		</form>
-		<p>Cards Remaining: {52 - getNextCard}</p>
+		<button onClick={drawNextCard}>Draw a Card</button>
+		<button onClick={toggleAutoDraw}>Toggle Auto-Draw</button>
+		<button onClick={shuffleDeck}>Get a New Deck</button>
+		<p>Cards Remaining: {cardsRemaining}</p>
 		<div id="card-container" ref={cardImageContainer}>
 
 		</div>
